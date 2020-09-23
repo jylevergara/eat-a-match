e54457c7daccc99753962d84917c379d8aa98476
Jyle Vergara <jylevergara@gmail.com>
2020-09-24 00:06:40 +0800

    initial commit (+1 squashed commit) Squashed commits: [f05d2a7] add major content

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ImageBackground, Text, View } from 'react-native';
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+import Demo from './assets/data/demo';
+import styles from './assets/styles';
+import CardItem from './components/CardItem';
+import City from './components/City';
+import Filters from './components/Filters';
+import Icon from './components/Icon';
+import HomeScreen from './containers/Home';
+import MatchesScreen from './containers/Matches';
+import MessagesScreen from './containers/Messages';
+import ProfileScreen from './containers/Profile';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      // tabBarOptions={{
+      //   //showIcon: true,
+      //   activeTintColor: '#7444C0',
+      //   inactiveTintColor: '#363636',
+      //   labelStyle: {
+      //     fontSize: 14,
+      //     textTransform: 'uppercase',
+      //     paddingTop: 10,
+      //   },
+      //   style: {
+      //     backgroundColor: '#FFF',
+      //     borderTopWidth: 0,
+      //     paddingVertical: 30,
+      //     height: 60,
+      //     marginBottom: 0,
+      //     shadowOpacity: 0.05,
+      //     shadowRadius: 10,
+      //     shadowColor: '#000',
+      //     shadowOffset: { height: 0, width: 0 },
+      //   },
+      // }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        // options={{
+        //   tabBarIcon: ({ focused }) => {
+        //     const iconFocused = focused ? '#7444C0' : '#363636';
+        //     return (
+        //       <Text style={[styles.iconMenu, { color: iconFocused }]}>
+        //         <Icon name="explore" />
+        //       </Text>
+        //     );
+        //   },
+        // }}
+      />
+      <Tab.Screen
+        name="Matches"
+        component={MatchesScreen}
+        // options={{
+        //   tabBarIcon: ({ focused }) => {
+        //     const iconFocused = focused ? '#7444C0' : '#363636';
+        //     return (
+        //       <Text style={[styles.iconMenu, { color: iconFocused }]}>
+        //         <Icon name="explore" />
+        //       </Text>
+        //     );
+        //   },
+        // }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={MessagesScreen}
+        // options={{
+        //   tabBarIcon: ({ focused }) => {
+        //     const iconFocused = focused ? '#7444C0' : '#363636';
+        //     return (
+        //       <Text style={[styles.iconMenu, { color: iconFocused }]}>
+        //         <Icon name="explore" />
+        //       </Text>
+        //     );
+        //   },
+        // }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        // options={{
+        //   tabBarIcon: ({ focused }) => {
+        //     const iconFocused = focused ? '#7444C0' : '#363636';
+        //     return (
+        //       <Text style={[styles.iconMenu, { color: iconFocused }]}>
+        //         <Icon name="explore" />
+        //       </Text>
+        //     );
+        //   },
+        // }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ assets/data/demo.js
@@ -0,0 +1,103 @@
+module.exports = [
+	{
+		id: 1,
+		name: 'Honey Butter Almond',
+		status: 'Online',
+		match: '78',
+		description: 'S&R',
+		message: '',
+		image: require('../images/almond.jpg')
+	},
+	{
+		id: 2,
+		name: 'Cinnamon Toast Crunch',
+		match: '93',
+		description: 'Find me at your nearest grocery store!',
+		status: 'Offline',
+		message: '',
+		image: require('../images/cereal.jpg')
+	},
+	{
+		id: 3,
+		name: 'Banoffee Crepe',
+		match: '45',
+		description: 'Cafe Breton',
+		status: 'Offline',
+		message: '',
+		image: require('../images/crepe.jpg')
+	},
+	{
+		id: 4,
+		name: 'Razon\'s Halo-halo',
+		match: '88',
+		description: 'Razon\'s',
+		status: 'Online',
+		message: "",
+		image: require('../images/halohalo.jpg')
+	},
+	{
+		id: 5,
+		name: 'Oreo Milktea',
+		match: '76',
+		description: 'Dakasi',
+		status: 'Offline',
+		message: "It's not who I am underneath but what I do that defines me.",
+		image: require('../images/milktea.jpg')
+	},
+	{
+		id: 6,
+		name: 'Cheesy Eggdesal',
+		match: '95',
+		description: 'McDonald\'s',
+		status: 'Online',
+		message: '',
+		image: require('../images/pan.jpg')
+	},
+	{
+		id: 7,
+		name: 'Smirnoff Mule',
+		match: '67',
+		description: 'Grocery store',
+		status: 'Online',
+		message: "",
+		image: require('../images/smirnoff.jpg')
+	},
+	{
+		id: 8,
+		name: 'Sweet and Sour Pork',
+		match: '85',
+		description:
+			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+		age: '27',
+		location: 'Irvine, CA',
+		// info1: 'Straight, Single, 5"10',
+		// info2: 'Tea Totaller, Loves Photography & Travel',
+		// info3: 'Beaches, Mountain, Cafe, Movies',
+		// info4: 'Last seen: 23h ago',
+		status: 'Online',
+		message: "",
+		image: require('../images/sweetsour.jpg')
+	},
+	{
+		id: 9,
+		name: 'Glenna Reichert',
+		match: '74',
+		description:
+			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+		status: 'Online',
+		message:
+			'This is what happens when an unstoppable force meets an immovable object.',
+		image: require('../images/09.jpg')
+	},
+	{
+		id: 10,
+		name: 'Kurtis DuBuque',
+		match: '98',
+		description:
+			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+		status: 'Offline',
+		message:
+			'You want order in Gotham. Batman must take off his mask and turn himself in.',
+		image: require('../images/10.jpg')
+	}
+];
